026e2e5b0e321329c4c09605508323c897d2db65
5h140w <5h140w23@gmail.com>
2024-07-30 20:46:37 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
 function App() {
+  const [tasks, settasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+  // Track lil tasks as a state
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TaskList tasks={tasks} settasks={settasks} />
+      <TaskForm tasks={tasks} settasks={settasks} />
+    </>
   );
 }
 
